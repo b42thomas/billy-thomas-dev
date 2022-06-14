6c1bd56edb6923440399ef739867cbdc5d7af447
Billy Thomas <b42thomas@gmail.com>
2022-06-14 12:37:15 -0400

    add contact info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
 <h1>Hi, I'm Billy Thomas</h1>
 
+<a href="mailto:billythomasdev@gmail.com" data-test=email>billythomasdev@gmail.com</a>
+<a href="https://www.linkedin.com/in/billy-thomas-60330012b/" target="blank" data-test=linkedinprofile>LinkedIn</a>
+<a href="https://github.com/b42thomas" target="blank" data-test=github>Github</a>
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,24 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
+	console.log(`Running ${testInfo.title}`);
 	await page.goto('/');
+});
+  
+test('index page has expected h1', async ({ page }) => {
 	expect(await page.textContent('h1')).toBe('Hi, I\'m Billy Thomas');
+});
+
+test('contact info is on page', async ({ page}) => {
+	var email = await page.locator('[data-test=email]');
+	expect(await email.textContent()).toBe('billythomasdev@gmail.com');
+	expect(await email.getAttribute('href')).toBe("mailto:billythomasdev@gmail.com");
+
+	var linkedInProfile =  await page.locator('data-test=linkedinprofile');
+	expect(await linkedInProfile.textContent()).toBe('LinkedIn');
+	expect(await linkedInProfile.getAttribute('href')).toBe('https://www.linkedin.com/in/billy-thomas-60330012b/');
+	
+	var github = await page.locator('data-test=github');
+	expect(await github.textContent()).toBe('Github');
+	expect(await github.getAttribute("href")).toBe('https://github.com/b42thomas');
 });

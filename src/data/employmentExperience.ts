884711314b73593734fdc424129901795937ccb2
Billy <b42thomas@gmail.com>
2024-07-10 15:59:54 -0400

    add humankind work experience

--- src/data/employmentExperience.ts
+++ src/data/employmentExperience.ts
@@ -1,10 +1,23 @@
 import type { EmploymentExperience } from "$lib/types/employmentExperience";
 
+const humankind: EmploymentExperience = {
+    employer: "HumanKind",
+    jobTitle: "Software Engineer",
+    startDate: new Date(2022, 8),
+    endDate: "Currently Employed",
+    technologies: ["svelte", "sveltekit", "AWS", "Github Actions", "AWS CDK", "AWS DynamoDB", "Opensearch"],
+    bulletPoints: [
+        "Built an internal tool with sveltekit for customer success team to manage client settings",
+        "Setup and maintained CI/CD pipelines for multiple frontend projects using Github Actions and AWS CDK",
+        "Integrated client product data from multiple APIs (Shopify, Salesforce, etc) for shopping cart builder",
+    ]
+}
+
 const afterSchoolHQ: EmploymentExperience = {
     employer: "AfterSchool HQ",
     jobTitle: "Software Engineer",
     startDate: new Date(2020, 8),
-    endDate: "Currently Employed",
+    endDate: new Date(2022, 8),
     technologies: ["C#", ".NET", "React", "AWS", "Azure Devops", "YAML", "SQL"],
     bulletPoints: [
         "Managed a remote team of overseas developers with multiple projects",
@@ -40,6 +53,7 @@ const developertTownStartsIntern: EmploymentExperience = {
 }
 
 const employmentHistory: readonly EmploymentExperience[] = [
+    humankind,
     afterSchoolHQ,
     developerTownStarts,
     developertTownStartsIntern
